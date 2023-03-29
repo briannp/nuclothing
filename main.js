22a94fc662b0ebc3bb8ead37fc942360fd38084d
Brian <brian.np@hotmail.com>
2023-03-28 23:23:09 -0300

    mejoras a responsive y mas backgrounds

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let randomChange = document.getElementById("randomChange")
-images = ['resources/concert.gif', 'resources/kconcert.gif', 'resources/cuerda.gif']
+images = ['resources/concert.gif', 'resources/kconcert.gif', 'resources/cuerda.gif' , 'resources/noise.gif' , 'resources/alkaline.gif', 'resources/nero.gif']
 
 
 let imgCount = images.length
