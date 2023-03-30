f10b5ad4178489848279d6b2da8e1437bd159bd0
Brian <brian.np@hotmail.com>
2023-03-29 21:33:43 -0300

    agregados navbar con animacion y arreglos en general

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// RANDOM BACKGROUND
+
 let randomChange = document.getElementById("randomChange")
 images = ['resources/concert.gif', 'resources/kconcert.gif', 'resources/cuerda.gif' , 'resources/alkaline.gif', 'resources/nero.gif']
 
@@ -9,3 +11,23 @@ let number = Math.floor(Math.random()* imgCount);
 window.onload = function (){
     randomChange.style.backgroundImage = "url("+images[number]+")"
 }
+
+
+// Navbar
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('.menu');
+let menuBtn = document.querySelector('.fa-bars');
+let closeBtn = document.querySelector('.fa-xmark');
+
+menuBtn.addEventListener("click", ()=> {
+    menu.classList.add('active');
+});
+
+closeBtn.addEventListener("click", ()=> {
+    menu.classList.remove('active');
+});
